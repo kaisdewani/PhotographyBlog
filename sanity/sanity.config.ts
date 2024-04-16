ba78a7c6c4874182af8aeee1cd6e9cae5976cb96
Kais <kaisdewani@gmail.com>
2024-04-16 13:46:08 -0500

    forgot another line to remove

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,6 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
